2d2361aedc731e5585e1565fd748a6876a0f79b2
Laura-VLN <lauravilain.pro@gmail.com>
2021-06-01 22:53:48 +0200

    onclick function added

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function share() {
+    var arrowShare = document.getElementById("arrow-share");
+
+    if (arrowShare.style.display === "none") {
+        arrowShare.style.display = "block";
+    } else {
+        arrowShare.style.display = "none";
+    }
+}
